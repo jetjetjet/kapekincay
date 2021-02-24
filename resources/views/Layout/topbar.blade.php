40c209bdcfd9378d3535c3f2d51f79f12933906e
Curos-M <sikampret091@gmail.com>
2021-02-24 23:46:24 +0700

    topbar

--- resources/views/Layout/topbar.blade.php
+++ resources/views/Layout/topbar.blade.php
@@ -80,7 +80,7 @@
 										<li><a href="{{ url('/order/meja/view') }}">Pesanan</a></li>
 									@endif
 									@if(Perm::can(['order_lihat']))
-										<li><a href="{{ url('/order/Index') }}">Daftar Pesanan</a></li>
+										<li><a href="{{ url('/order/index') }}">Daftar Pesanan</a></li>
 									@endif
 									@if(Perm::can(['pengeluaran_lihat']))
 										<li><a href="{{ url('/pengeluaran') }}">Pengeluaran</a></li>
@@ -102,6 +102,30 @@
 								<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
 							</a>
 						</li>
+						@endif
+						@if(Perm::can(['order_lihat']) || Perm::can(['pengeluaran_lihat']))
+							<?php
+								$segm = (Request::segment(1) == 'order' || Request::segment(1) == 'pengeluaran');
+							?>
+							<li class="menu single-menu {{ $segm ? 'active' : ''}}">
+								<a href="#report" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+									<div class="">
+
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text shadow-icons"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+										<span>Laporan</span>
+									</div>
+									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+								</a>
+								<ul class="collapse submenu list-unstyled animated fadeInUp" id="report" data-parent="#topAccordion">
+									@if(Perm::can(['order_lihat']))
+										<li><a href="{{ url('/laporan') }}">Laporan Transaksi</a></li>
+									@endif
+									@if(Perm::can(['order_lihat']))
+										<li><a href="{{ url('/log') }}">Log</a></li>
+									@endif
+								</ul>
+							</li>
 						@endif
 						@if(Perm::can(['pengaturan_lihat']) || Perm::can(['pengaturan_backupdb']))
 							<?php
