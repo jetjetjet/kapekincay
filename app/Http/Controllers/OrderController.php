1dc99da02366e35c9d0ae5611cdaee767c9c94ac
Curos-M <sikampret091@gmail.com>
2021-04-07 22:49:26 +0700

    dd tes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -77,6 +77,8 @@ class OrderController extends Controller
     $respon = Helpers::$responses;
     $results = OrderRepository::getOrder($respon, $id);
 
+		$dd($results);
+
 		if($results['status'] == 'error'){
 			$request->session()->flash($results['status'], $results['messages']);
 			return redirect()->action([OrderController::class, 'orderView']);
