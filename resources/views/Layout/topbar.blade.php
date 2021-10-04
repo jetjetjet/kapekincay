ecf35159bb9c525c5bf797386c6a893f69665391
MufidAqil <sikampret091@gmail.com>
2021-10-04 18:49:14 +0700

    tinggal

--- resources/views/Layout/topbar.blade.php
+++ resources/views/Layout/topbar.blade.php
@@ -9,11 +9,6 @@
 			<div class="topbar-nav header navbar" role="banner">
 				<nav id="topbar">
 					<ul class="navbar-nav theme-brand flex-row text-center">
-						<li class="nav-item theme-logo">
-							<a href="index.html">
-								<img src="{{ url('/') }}/assets/img/90x90.jpg" class="navbar-logo" alt="logo">
-							</a>
-						</li>
 						<li class="nav-item theme-text">
 							<a href="index.html" class="nav-link"> {{ session('cafeName') }} </a>
 						</li>
