c9ae9fba0a1990d12b7965efbd0d9b76c4cbfc13
MufidAqil <sikampret091@gmail.com>
2021-10-04 17:09:29 +0700

    font

--- resources/views/Layout/index.blade.php
+++ resources/views/Layout/index.blade.php
@@ -12,7 +12,7 @@
     <script src="{{ url('/') }}/assets/js/loader.js"></script>
 
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+    <link href="{{ url('/') }}/font/font.css" rel="stylesheet" type="text/css">
     <link href="{{ url('/') }}/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="{{ url('/') }}/assets/css/plugins.css" rel="stylesheet" type="text/css" />
     <link href="{{ url('/') }}/plugins/sweetalerts/sweetalert2.min.css" rel="stylesheet" type="text/css" />
